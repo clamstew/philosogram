05254eff46f8be28a1e4e5ecee61ff9b2434429c
Clay Stewart <claytonmstewart@gmail.com>
2013-11-06 22:59:01 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Philosogram::Application.config.secret_key_base = '0592cf49b32a44edddbf034e44c4eb35de989eb53d817c4764c1a47b5badaaf422329716dc5ca342b7a2bc946b9230588b9522649aca9106f557259ee438d546'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Philosogram::Application.config.session_store :cookie_store, key: '_philosogram_session'
